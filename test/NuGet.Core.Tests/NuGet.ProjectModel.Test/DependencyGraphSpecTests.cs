e2d21417b57ae95a32f50c212c448ae2456f1b5b
Justin Emgarten <justin@emgarten.com>
2016-09-06 18:04:40 -0700

    tests

--- test/NuGet.Core.Tests/NuGet.ProjectModel.Test/DependencyGraphSpecTests.cs
+++ test/NuGet.Core.Tests/NuGet.ProjectModel.Test/DependencyGraphSpecTests.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace NuGet.ProjectModel.Test
 {
     public class DependencyGraphSpecTests
     {
+        [Fact]
+        public void DependencyGraphSpec_ReadEmptyJObject()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
     }
 }
